3f005a9f7ee3c4897f27583942090325d0a6dfbf
Daniel Vazquez <daniel.vazquez@ascendmakers.com>
2025-03-10 20:47:01 +0100

    footer fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,20 @@ const Footer = () => {
   const currentYear = dayjs().year();
   return (
     <div className='footerWrapper'>
+
+      <div className='addBox'>
+
+      <p className='tit'>Our Address</p>
+      <p>Calle 55, PH SFC. <br />Oficina 26D Obarrio, <br />Panama City, Panamá</p>
+      <p className='email'>info@pfd-international.com</p>
+
+      </div>
+
+      <div className='rigBox'>
         <p><span>{`${currentYear} ©`}</span> All rights reserved.</p>
+        <div className='box' />
+
+      </div>
     </div>
   )
 }
