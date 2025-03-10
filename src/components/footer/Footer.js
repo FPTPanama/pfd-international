664d824659f82ff4e7a35a6713b0e628d75207e8
Daniel Vazquez <daniel.vazquez@ascendmakers.com>
2025-03-10 20:16:40 +0100

    footer add

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   const currentYear = dayjs().year();
   return (
     <div className='footerWrapper'>
-        <p><span>{`${currentYear} ©`}</span>  All rights reserved.</p>
+        <p><span>{`${currentYear} ©`}</span> All rights reserved.</p>
     </div>
   )
 }
